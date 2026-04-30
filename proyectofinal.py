dec667f02eff03cb8688c16c8549279de1db8d05
Abigailreza <carlosabigaildesiree@gmail.com>
2026-04-29 19:24:24 -0600

    Add files via upload
    programa python pantalla principal

---
+++ proyectofinal.py
@@ -0,0 +1,96 @@
+import tkinter as tk
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+
+# -------------------------
+# FUNCIONES
+# -------------------------
+def abrir_registro_productos():
+   messagebox.showinfo("Registro de Productos", "Aquí irá el módulo de registro de productos.")
+
+def abrir_registro_ventas():
+   messagebox.showinfo("Registro de Ventas", "Aquí irá el módulo de registro de ventas.")
+
+def abrir_reportes():
+   messagebox.showinfo("Reportes", "Aquí irá el módulo de reportes.")
+
+def abrir_acerca_de():
+   messagebox.showinfo("Acerca de", "Punto de Venta de Ropa\nProyecto Escolar\nVersión 1.0")
+
+
+# -------------------------
+# VENTANA PRINCIPAL
+# -------------------------
+ventana = tk.Tk()
+ventana.title("Punto de Venta - software punto de venta abigailreza v1")
+ventana.geometry("500x600")
+ventana.resizable(False, False)
+
+
+# -------------------------
+# LOGO
+# -------------------------
+try:
+   BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+   imagen = Image.open(os.path.join(BASE_DIR,"logo.png"))
+   imagen = imagen.resize((250, 250))
+   img_logo = ImageTk.PhotoImage(imagen)
+
+   lbl_logo = tk.Label(ventana, image=img_logo)
+   lbl_logo.pack(pady=20)
+except:
+   lbl_sin_logo = tk.Label(ventana, text="(Aquí va el logo del sistema)", font=("Arial", 14))
+   lbl_sin_logo.pack(pady=40)
+
+
+# -------------------------
+# ESTILO BOTONES
+# -------------------------
+color_boton = "#ff69b4"      # rosa
+color_texto = "#ffb6c1"      # rosa más claro
+
+# -------------------------
+# BOTONES PRINCIPALES
+# -------------------------
+btn_reg_prod = tk.Button(
+    ventana, text="Registro de Productos",
+    command=abrir_registro_productos,
+    bg=color_boton, fg=color_texto,
+    font=("Arial", 12),
+    width=25, height=2
+)
+btn_reg_prod.pack(pady=10)
+
+btn_reg_ventas = tk.Button(
+    ventana, text="Registro de Ventas",
+    command=abrir_registro_ventas,
+    bg=color_boton, fg=color_texto,
+    font=("Arial", 12),
+    width=25, height=2
+)
+btn_reg_ventas.pack(pady=10)
+
+btn_reportes = tk.Button(
+    ventana, text="Reportes",
+    command=abrir_reportes,
+    bg=color_boton, fg=color_texto,
+    font=("Arial", 12),
+    width=25, height=2
+)
+btn_reportes.pack(pady=10)
+
+btn_acerca = tk.Button(
+    ventana, text="Acerca de",
+    command=abrir_acerca_de,
+    bg=color_boton, fg=color_texto,
+    font=("Arial", 12),
+    width=25, height=2
+)
+btn_acerca.pack(pady=10)
+
+
+# -------------------------
+# INICIO DE LA APP
+# -------------------------
+ventana.mainloop()
